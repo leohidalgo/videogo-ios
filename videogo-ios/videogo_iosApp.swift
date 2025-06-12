8d85f71a7c1e0c8ef099a5cd7571a7340631af50
leohidalgo <leohidalgo@protonmail.com>
2025-06-12 13:41:42 -0400

    feat: add Xcode base project

---
+++ videogo-ios/videogo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  videogo_iosApp.swift
+//  videogo-ios
+//
+//  Created by leohidalgo on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct videogo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
