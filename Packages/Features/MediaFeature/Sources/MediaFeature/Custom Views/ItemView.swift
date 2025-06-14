b4631d98da2ed1e5149f0085d15da42e9c17540d
leohidalgo <leohidalgo@protonmail.com>
2025-06-14 11:46:00 -0400

    feat(media-feature): ItemView handle image error (#11)

--- Packages/Features/MediaFeature/Sources/MediaFeature/Custom Views/ItemView.swift
+++ Packages/Features/MediaFeature/Sources/MediaFeature/Custom Views/ItemView.swift
@@ -2,27 +2,36 @@ import SwiftUI
 
 struct ItemView: View {
 
-    private let title: String?
+    private let title: String
     private let image: URL
 
     var body: some View {
-        ZStack {
-            AsyncImage(url: image) { image in
+        AsyncImage(url: image) { phase in
+            if let image = phase.image {
                 image.resizable()
-            } placeholder: {
+            } else if phase.error != nil {
+                // TODO: Track this error with Crashlytics
+
+                ZStack {
+                    Color.blue
+                    Text(title)
+                        .foregroundStyle(.white)
+                        .font(.caption)
+                }
+            } else {
                 ZStack {
                     Rectangle()
                         .fill(.gray.opacity(0.3))
                     ProgressView()
                 }
             }
-            .cornerRadius(10)
-            .frame(width: 100, height: 150)
         }
+        .cornerRadius(10)
+        .frame(width: 100, height: 150)
         .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 4)
     }
 
-    init(title: String? = nil, image: URL) {
+    init(title: String, image: URL) {
         self.title = title
         self.image = image
     }
