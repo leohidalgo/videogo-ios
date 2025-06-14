6859b99148fbe06129bebc25313c5f43c3f8dad0
leohidalgo <leohidalgo@protonmail.com>
2025-06-14 12:01:12 -0400

    feat(media-player): improve mediamodel.id (#13)

--- Packages/Features/MediaFeature/SnapshotTests/GalleryViewSnapshotTests.swift
+++ Packages/Features/MediaFeature/SnapshotTests/GalleryViewSnapshotTests.swift
@@ -45,6 +45,7 @@ private extension Helper {
         let defaultURL = try #require(URL(string: "http://example.com"))
 
         return MediaModel(
+            id: UUID().uuidString,
             title: title,
             description: description,
             image: image ?? defaultURL,
--- Packages/Features/MediaFeature/Sources/MediaFeature/Models/MediaModel.swift
+++ Packages/Features/MediaFeature/Sources/MediaFeature/Models/MediaModel.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 struct MediaModel {
-    let id: UUID
+    let id: String
     let category: CategoryKind
     let description: String?
     let title: String
     let image: URL
     let video: URL?
 
-    init(id: UUID = UUID(), title: String, description: String?, image: URL, video: URL?, category: CategoryKind) {
+    init(id: String, title: String, description: String?, image: URL, video: URL?, category: CategoryKind) {
         self.id = id
         self.title = title
         self.description = description
